bbc9b9a431b645a1b7112c7c9b6629f12945dace
JGVR <juan@galileolegal.ai>
2024-08-14 16:55:38 -0400

    fixed nativewind configuration issues

--- stbc-ui/app/home/details.tsx
+++ stbc-ui/app/home/details.tsx
@@ -1,14 +1,23 @@
-import {Text, View} from 'react-native';
-import {styled} from 'nativewind';
+import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 
-export default function DetailScreen(){
+export default function DetailScreen() {
     return (
-        <StyledView className="flex-row justify-between">
-            <StyledText>This is the details page</StyledText>
-            <StyledText className="left-2">This is the details page1111</StyledText>
+        <StyledView className="bg-blue-500 h-full">
+            <StyledView className='flex-row justify-between flex-wrap mt-4'>
+                <StyledView className="bg-black w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+                <StyledView className="bg-red-400 w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+                <StyledView className="bg-yellow-300 w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+                <StyledView className="bg-yellow-300 w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+                <StyledView className="bg-yellow-300 w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+                <StyledView className="bg-yellow-300 w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+                <StyledView className="bg-yellow-300 w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+                <StyledView className="bg-yellow-300 w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+                <StyledView className="bg-yellow-300 w-1/2 h-56 flex-none max-w-[46%] m-2"/>
+            </StyledView>
         </StyledView>
     );
 }
