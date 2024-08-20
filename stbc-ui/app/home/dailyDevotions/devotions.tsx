8c5c6290844a058d185e1ff1f91b0d8943af68bc
JGVR <juan@galileolegal.ai>
2024-08-20 13:43:15 -0400

    added navigation cards to the DailyDevotion screen

---
+++ stbc-ui/app/home/dailyDevotions/devotions.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function DevotionScreen(){
+    return(
+        <View>
+            <Text>Test</Text>
+        </View>
+    );
+}
--- stbc-ui/app/home/dailyDevotions/index.tsx
+++ stbc-ui/app/home/dailyDevotions/index.tsx
@@ -1,13 +1,14 @@
-import {Text, View} from 'react-native';
+import {ScrollView} from 'react-native';
 import {styled} from 'nativewind';
+import ThumbnailNavigationCard from '@/components/ThumbnailNavigationCard';
+import navigationCards from '@/utils/DevotionNavigationCards';
 
-const StyledView = styled(View);
-const StyledText = styled(Text);
+const StyledView = styled(ScrollView);
 
 export default function DailyDevotionsScreen(){
     return(
-        <StyledView>
-            <StyledText>Test</StyledText>
+        <StyledView className='h-full w-full bg-gray-800'>
+            <ThumbnailNavigationCard navigationCards={navigationCards}/>
         </StyledView>
     );
 }
