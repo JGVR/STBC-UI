44fc7bf048c9deb808d2b6de101c09a75bd83f17
JGVR <juangabrielvasquez11@gmail.com>
2024-09-03 12:36:03 -0400

    removed the isCompleted prop from the ThumbnailNavigationCard component

--- stbc-ui/components/ThumbnailNavigationCard.tsx
+++ stbc-ui/components/ThumbnailNavigationCard.tsx
@@ -4,7 +4,7 @@ import {styled} from  'nativewind';
 
 const StyledView = styled(View);
 
-export default function ThumbnailNavigationCard(props: {navigationCards: {title: string, imageDetail: any, screenUrl: string, imageLayout: {width: string, height: string, border: string, top: string, bottom: string, left: string, right: string}, textLayout: {width: string, height: string, position: string, top: string, bottom: string, right: string, left: string, textSize: string, textColor:string}}[], isCompleted: boolean}){
+export default function ThumbnailNavigationCard(props: {navigationCards: {title: string, imageDetail: any, screenUrl: string, imageLayout: {width: string, height: string, border: string, top: string, bottom: string, left: string, right: string}, textLayout: {width: string, height: string, position: string, top: string, bottom: string, right: string, left: string, textSize: string, textColor:string}}[]}){
     return(
         <StyledView className="flex-row flex-wrap justify-between mt-4">
             {
