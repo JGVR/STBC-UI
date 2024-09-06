fdfc620db3ca088ecb43bd8ff48df6f3d39610b0
JGVR <juangabrielvasquez11@gmail.com>
2024-09-05 22:16:52 -0400

    completed header in the event tab. Also added background color to the event tab

--- stbc-ui/app/events/index.tsx
+++ stbc-ui/app/events/index.tsx
@@ -1,10 +1,13 @@
-import {View, Text, StatusBar} from 'react-native';
+import {View, StatusBar, Text} from 'react-native';
 import BgImageScreenHeader from '@/components/headers/BgImageScreenHeader';
 import { useRouter } from 'expo-router';
 import ComponentLayout from '@/utils/ComponentLayout';
 import { useCallback } from 'react';
 import { useFocusEffect } from 'expo-router';
+import { styled } from 'nativewind';
 
+const StyledView = styled(View);
+const StyledText = styled(Text);
 const containerLayout = new ComponentLayout({height:"h-60", width:"w-full", color:"bg-dark-green"});
 const imageLayout = new ComponentLayout({height: "h-40", width: "w-96", left:"left-6", top:"top-28", border:"border rounded-2xl"});
 const titleLayout = new ComponentLayout({height:"", width:"", bottom:"bottom-28", size:"text-xl", color:"text-white"});
@@ -26,8 +29,11 @@ export default function EventsScreen(){
     );
 
     return(
-        <View>
+        <StyledView className="bg-midnight-green">
             <BgImageScreenHeader router={router} backButtonShown={false} buttonTitle="" backButtonLayout="" backIconLayout="" headerTitle='Upcoming Events' headerOptionalMsg="" imageLayout={imageLayout} containerLayout={containerLayout} titleLayout={titleLayout} optionalMsgLayout={optionalMsgLayout}/>
-        </View>
+            <StyledView className='bg-midnight-green h-3/4 w-full top-8'>
+                <StyledText className="text-white">Hello</StyledText>
+            </StyledView>
+        </StyledView>
     );
 }
