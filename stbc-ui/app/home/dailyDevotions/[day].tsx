01d279c684b92bca2eb5c96fd77431a7b65a50aa
JGVR <juangabrielvasquez11@gmail.com>
2024-09-13 14:00:06 -0400

    made some changes to the [day] dynamic page in the dailyDevotions screen to extract the appropriate event based on which date of the week the user clicks on

--- stbc-ui/app/home/dailyDevotions/[day].tsx
+++ stbc-ui/app/home/dailyDevotions/[day].tsx
@@ -5,6 +5,7 @@ import LoadingScreen from '@/components/loadingScreen';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import BgImageScreenHeader from '@/components/headers/BgImageScreenHeader';
 import ComponentLayout from '@/utils/ComponentLayout';
+import Devotion from '@/utils/Devotion';
 
 const StyledView = styled(View);
 const StyledScrollView = styled(ScrollView);
@@ -13,14 +14,14 @@ const containerLayout = new ComponentLayout({height:"h-60", width:"w-full"});
 const iconLayout = new ComponentLayout({height:"", width:"", top:"top-14", left:"left-3", color:"#0E4749"});
 const buttonLayout = new ComponentLayout({height:"", width:"", top:"top-12", right:"right-[5%]", color:"#0E4749"});
 const imageLayout = new ComponentLayout({height:"h-64", width:"w-full", opacity:"opacity-80"});
-const titleLayout = new ComponentLayout({height:"", width:"", bottom:"bottom-40", size:"text-2xl", color:"text-midnight-green"});
+const titleLayout = new ComponentLayout({height:"", width:"", bottom:"bottom-36", size:"text-2xl", color:"text-midnight-green"});
 const optionalMsgLayout = new ComponentLayout({height:"", width:"", bottom:"bottom-36", color: "text-midnight-green"});
 
 export default function DevotionScreen(){
     const [isCompleted, setIsCompleted] = useState(false);
-    const [devotions, setDevotions] = useState([]);
-    const url = "http://172.20.10.2:8000/find?type=devotion&churchId=1";
+    const [devotions, setDevotions] = useState({title: " ", memberId: " ", message: " "});
     const {day} = useLocalSearchParams();
+    const url = `http://192.168.1.12:8000/find?type=devotion&churchId=1&weekDay=${day}`;
     const router = useRouter();
     
     //Extract the correct devotion from STBC-Api
@@ -33,7 +34,11 @@ export default function DevotionScreen(){
                 return response.json();
             }
         ).then(data => {
-            setDevotions(data);
+            setDevotions({
+                title: data[0].title,
+                memberId: data[0].memberId,
+                message: data[0].message
+            });
             setIsCompleted(true);
         })
         .catch(error => {
@@ -45,11 +50,11 @@ export default function DevotionScreen(){
     if(isCompleted){
         return(
             <StyledView className='h-[100%]'>
-                <BgImageScreenHeader router={router} buttonTitle='Devotions' headerTitle={devotions.length > 0 ? devotions[1]["title"] : ""} headerOptionalMsg={devotions.length > 0 ? devotions[1]["memberId"] : null} containerLayout={containerLayout} backButtonLayout={buttonLayout} backIconLayout={iconLayout} backButtonShown={true} imageLayout={imageLayout} titleLayout={titleLayout} optionalMsgLayout={optionalMsgLayout}/>
+                <BgImageScreenHeader router={router} buttonTitle='Devotions' headerTitle={devotions.title} headerOptionalMsg={devotions.memberId} containerLayout={containerLayout} backButtonLayout={buttonLayout} backIconLayout={iconLayout} backButtonShown={true} imageLayout={imageLayout} titleLayout={titleLayout} optionalMsgLayout={optionalMsgLayout}/>
                 <StyledScrollView className="w-full h-80 bg-dark-green border rounded-tr-2xl rounded-tl-2xl overflow-hidden">
                     <StyledView className="w-full">
-                        <StyledText className="text-white m-3 text-center italic font-bold text-lg">
-                            {`${devotions[1]["message"]}`}
+                        <StyledText className="text-white m-3 text-center text-base">
+                            {`${devotions.message}`}
                         </StyledText>
                     </StyledView>
                 </StyledScrollView>
