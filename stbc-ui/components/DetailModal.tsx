3720503d5405c632ce403bb44af451364ecc7e53
JGVR <juangabrielvasquez11@gmail.com>
2024-10-11 16:36:50 -0400

    added a close icon to the DetailModal and further enhanced the style of the modal

--- stbc-ui/components/DetailModal.tsx
+++ stbc-ui/components/DetailModal.tsx
@@ -1,7 +1,6 @@
 import {View, Text, Modal, ScrollView, Image, Pressable} from 'react-native';
 import { styled } from 'nativewind';
-import { useState } from 'react';
-import Member from '@/model/Member';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
@@ -14,12 +13,12 @@ export default function DetailModal(props: {member: any, isVisible: boolean, set
         <Modal visible={props.isVisible} transparent={true} animationType='slide'>
                 <StyledView className="bg-dark-green bg-opacity-50 h-[60%] w-96 mt-40 ml-5 rounded-lg">
                     <StyledView className='flex-row'>
-                        <StyledText className="text-white text-lg font-bold mb-4 ml-32 mt-2">{props.member.current?.firstName} {props.member.current?.lastName}</StyledText>
-                        <StyledPressable className='mt-2 ml-20' onPress={() => props.setIsVisible(false)}>
-                            <StyledText className="text-teal-500 text-lg font-bold italic">Close</StyledText>
+                        <StyledText className="text-white text-center text-lg font-bold mb-4 ml-24 mt-2 w-52">{props.member.current?.firstName} {props.member.current?.lastName}</StyledText>
+                        <StyledPressable className='mt-2 ml-[8%]' onPress={() => props.setIsVisible(false)}>
+                            <AntDesign name="closecircleo" size={38} color="#14B8A6" />
                         </StyledPressable>
                     </StyledView>
-                    <StyledImage className="h-48 w-48 ml-[24%]" src={props.member.current?.imageUrl} />
+                    <StyledImage className="h-48 w-48 ml-[27%]" src={props.member.current?.imageUrl} />
                     <StyledScrollView>
                         <StyledText className="m-6 text-white text-base text-left leading-7">This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go. This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go. This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go. This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go. This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go. This is where the bio of the church member will go.This is where the bio of the church member will go.This is where the bio of the church member will go.
                         </StyledText>
