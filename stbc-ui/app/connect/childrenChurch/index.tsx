65646093529519a7624af4d14b235a05b53b61ae
JGVR <juangabrielvasquez11@gmail.com>
2024-10-13 22:18:44 -0400

    added logic to extract Children's church data by calling the API

--- stbc-ui/app/connect/childrenChurch/index.tsx
+++ stbc-ui/app/connect/childrenChurch/index.tsx
@@ -1,11 +1,50 @@
 import {View, Text, Image, ScrollView} from 'react-native';
 import { styled } from 'nativewind';
+import { useState, useEffect } from 'react';
+import School from '@/model/School';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledScrollView = styled(ScrollView);
+const apiUrl = "http://192.168.1.12:8000/find?type=school&churchId=1&schoolId=1";
 
 export default function ChildrenChurchScreen(){
+    const [school, setSchool] = useState<School>();
+    const [isCompleted, setIsCompleted] = useState(false);
+
+    const fetchSchool = async() => {
+        try{
+            const resp = await fetch(apiUrl);
+
+            if(!resp.ok){
+                throw new Error("Something went wrong with the API request");
+            }
+            const data = await resp.json();
+
+            const childrenSchool = data.map((school: any) => {
+                const newSchool = new School({
+                    id: school["schoolId"],
+                    name: school["name"],
+                    shortDescription: school["shortDescription"],
+                    description: school["description"],
+                    dateOfWeek: school["dateOfWeek"],
+                    time: school["time"],
+                    imageUrl: school["imageUrl"],
+                    classes: school["classes"]
+                });
+                return newSchool;
+            });
+            setSchool(childrenSchool);
+            setIsCompleted(true);
+        }catch(error){
+            console.log("something went wrong!" + error);
+        }
+    };
+
+    useEffect(() => {
+        fetchSchool();
+    }, []);
+
     return(
         <StyledScrollView>
             <StyledText>Hello!</StyledText>
