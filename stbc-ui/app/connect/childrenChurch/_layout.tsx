5a222fcd9b301a0f5ad7847869820300d9981de7
JGVR <juangabrielvasquez11@gmail.com>
2024-10-13 23:00:41 -0400

    added BgImageScreenHeader to the childrenChurch screen

--- stbc-ui/app/connect/childrenChurch/_layout.tsx
+++ stbc-ui/app/connect/childrenChurch/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 export default function ChildrenChurchLayout(){
     return(
         <Stack>
-            <Stack.Screen name='index'/>
+            <Stack.Screen name='index' options={{
+                headerShown: false
+            }}/>
         </Stack>
     );
 }
--- stbc-ui/app/connect/childrenChurch/index.tsx
+++ stbc-ui/app/connect/childrenChurch/index.tsx
@@ -1,16 +1,29 @@
 import {View, Text, Image, ScrollView} from 'react-native';
 import { styled } from 'nativewind';
+import { useRouter } from 'expo-router';
 import { useState, useEffect } from 'react';
 import School from '@/model/School';
+import BgImageScreenHeader from '@/components/headers/BgImageScreenHeader';
+import ComponentLayout from '@/utils/ComponentLayout';
+import SectionHeader from '@/components/SectionHeader';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledScrollView = styled(ScrollView);
 const apiUrl = "http://192.168.1.12:8000/find?type=school&churchId=1&schoolId=1";
 
+const containerLayout = new ComponentLayout({height:"h-60", width:"w-full"});
+const subContainerLayout = new ComponentLayout({height: "", width: ""});
+const iconLayout = new ComponentLayout({height:"", width:"", bottom:"bottom-[48.5%]", left:"left-3", color:"white"});
+const buttonLayout = new ComponentLayout({height:"", width:"", bottom:"bottom-[50.5%]", right:"right-[5%]", color:"white"});
+const imageLayout = new ComponentLayout({height:"h-64", width:"w-full", opacity:"opacity-70", bottom:"bottom-1"});
+const titleLayout = new ComponentLayout({height:"", width:"", bottom:"bottom-48", size:"text-4xl", color:"text-white"});
+const optionalMsgLayout = new ComponentLayout({height:"", width:"", bottom:"bottom-48", left:"left-36", size:"text-lg", color: "text-white"});
+
 export default function ChildrenChurchScreen(){
-    const [school, setSchool] = useState<School>();
+    const [school, setSchool] = useState<School[]>([]);
     const [isCompleted, setIsCompleted] = useState(false);
+    const router = useRouter();
 
     const fetchSchool = async() => {
         try{
@@ -46,8 +59,9 @@ export default function ChildrenChurchScreen(){
     }, []);
 
     return(
-        <StyledScrollView>
-            <StyledText>Hello!</StyledText>
+        <StyledScrollView className='bg-dark-green w-full h-full'>
+            <BgImageScreenHeader router={router} imageUrl="https://stbc.blob.core.windows.net/stbc-mobile-app-images/Devotion-bg-Image.webp" buttonTitle='Sunday Class' headerTitle={school[0]?.name ? school[0].name : "ok"} headerOptionalMsg={school[0]?.dateOfWeek && school[0]?.time ? `${school[0].dateOfWeek}|${school[0].time}` : ""} containerLayout={containerLayout} subContainerLayout={subContainerLayout} backButtonLayout={buttonLayout} backIconLayout={iconLayout} backButtonShown={true} imageLayout={imageLayout} titleLayout={titleLayout} optionalMsgLayout={optionalMsgLayout}/>
+            <SectionHeader title='Leaders' containerLayout='flex-row flex-nowrap' titleLayout='text-2xl text-white mt-8 mb-2 ml-4 font-bold italic' iconLayout='mt-9'/>
         </StyledScrollView>
     );
 }
