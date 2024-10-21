32d2fcacd29b3887f42e617145234b6a6af0cd06
JGVR <juangabrielvasquez11@gmail.com>
2024-10-21 12:59:10 -0400

    modified algorithm to extract stbc youtube video data and create and object for each video extracted. The custom video object will be used in the Media screen

--- stbc-ui/app/media/index.tsx
+++ stbc-ui/app/media/index.tsx
@@ -1,15 +1,20 @@
 import {View, Text, Image} from 'react-native';
 import { styled } from 'nativewind';
-import YoutubeChannel from '@/model/YoutubeChannel';
 import { useState, useEffect } from 'react';
+import Video from '@/model/Video';
+import YoutubeChannel from '@/model/YoutubeChannel';
+import LoadingScreen from '@/components/loadingScreen';
+import ItemsList from '@/components/ItemsList';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
+const StyledImage = styled(Image);
 
 export default function MediaScreen(){
     const channel = new YoutubeChannel({id:process.env.EXPO_PUBLIC_STBC_CHANNEL_ID, url: process.env.EXPO_PUBLIC_YOUTUBE_API});
     const youtubeApiUrl = `${channel.url}&channelId=${channel.id}&maxResults=5&order=date&key=${process.env.EXPO_PUBLIC_YOUTUBE_API_KEY}`;
-    const [videos, setVideos] = useState([]);
+    const [videos, setVideos] = useState<Video[]>([]);
+    const [isCompleted, setIsCompleted] = useState(false);
 
     const fetchVideos = async() => {
         try{
@@ -20,9 +25,17 @@ export default function MediaScreen(){
             }
 
             const data = await resp.json();
-            console.log(data);
-            console.log(data["items"][0]["id"]["videoId"]);
-
+            const videos = data["items"].map((video: any) => {
+                const newVideo = new Video({
+                    id: video["id"]["videoId"],
+                    title: video["snippet"]["title"],
+                    description: video["snippet"]["description"],
+                    thumbNailUrl: video["snippet"]["thumbnails"]["default"]["url"]
+                });
+                return newVideo;
+            });
+            setVideos(prevData => [...prevData, ...videos]);
+            setIsCompleted(true);
         }catch(error){
             console.log(`Something went wrong ${error}`);
         }
@@ -32,9 +45,12 @@ export default function MediaScreen(){
         fetchVideos();
     }, [])
 
-    return(
-        <StyledView>
-            <StyledText>Hello!</StyledText>
-        </StyledView>
-    );
+    if(isCompleted){
+        return(
+            <StyledView>
+                <StyledText>Hello!</StyledText>
+            </StyledView>
+        );
+    }
+    return <LoadingScreen/>;
 }
--- stbc-ui/model/Video.ts
+++ stbc-ui/model/Video.ts
@@ -3,7 +3,7 @@ interface VideoStruct{
     title: string,
     description: string,
     thumbNailUrl: string,
-    speaker: string
+    //speaker: string
 }
 
 export default class Video{
@@ -11,13 +11,13 @@ export default class Video{
     title: string
     description: string
     thumbNailUrl: string
-    speaker: string
+    //speaker: string
 
-    constructor({id, title, description, thumbNailUrl, speaker}: VideoStruct){
+    constructor({id, title, description, thumbNailUrl}: VideoStruct){
         this.id=id
         this.title=title
         this.description=description
         this.thumbNailUrl=thumbNailUrl
-        this.speaker=speaker
+        //this.speaker=speaker
     }
 }
