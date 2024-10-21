740c7bf72b1085fc153a2ecdc592e2cbcd02b24d
JGVR <juangabrielvasquez11@gmail.com>
2024-10-20 23:59:16 -0400

    added logic to extract video data from youtube API. I still need to map the data to my custom objects and used them within my components

--- stbc-ui/app/media/index.tsx
+++ stbc-ui/app/media/index.tsx
@@ -1,10 +1,37 @@
 import {View, Text, Image} from 'react-native';
 import { styled } from 'nativewind';
+import YoutubeChannel from '@/model/YoutubeChannel';
+import { useState, useEffect } from 'react';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 
 export default function MediaScreen(){
+    const channel = new YoutubeChannel({id:process.env.EXPO_PUBLIC_STBC_CHANNEL_ID, url: process.env.EXPO_PUBLIC_YOUTUBE_API});
+    const youtubeApiUrl = `${channel.url}&channelId=${channel.id}&maxResults=5&order=date&key=${process.env.EXPO_PUBLIC_YOUTUBE_API_KEY}`;
+    const [videos, setVideos] = useState([]);
+
+    const fetchVideos = async() => {
+        try{
+            const resp = await fetch(youtubeApiUrl);
+
+            if(!resp.ok){
+                throw new Error(`${resp.status} ${resp.statusText}`);
+            }
+
+            const data = await resp.json();
+            console.log(data);
+            console.log(data["items"][0]["id"]["videoId"]);
+
+        }catch(error){
+            console.log(`Something went wrong ${error}`);
+        }
+    };
+
+    useEffect(() => {
+        fetchVideos();
+    }, [])
+
     return(
         <StyledView>
             <StyledText>Hello!</StyledText>
--- stbc-ui/types/env.d.ts
+++ stbc-ui/types/env.d.ts
@@ -3,5 +3,6 @@ declare namespace NodeJS {
         EXPO_PUBLIC_STBC_API: string
         EXPO_PUBLIC_YOUTUBE_API: string
         EXPO_PUBLIC_STBC_CHANNEL_ID: string
+        EXPO_PUBLIC_YOUTUBE_API_KEY: string
     }
 }
