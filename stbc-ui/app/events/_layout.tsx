5372e39615da688efc847e93510a12865a320b0b
JGVR <juangabrielvasquez11@gmail.com>
2024-12-02 13:40:41 -0500

    temporary fixed layout issues in some screens. I need to implement a long-term solution

--- stbc-ui/app/events/_layout.tsx
+++ stbc-ui/app/events/_layout.tsx
@@ -3,7 +3,7 @@ import GeneralScreenHeader from "@/components/headers/GeneralScreenHeader";
 import ComponentLayout from "@/utils/ComponentLayout";
 
 const iconLayout = new ComponentLayout({height:"", width:"", top:"top-14", left:"left-3", color:"#0E4749"});
-const buttonLayout = new ComponentLayout({height:"", width:"", top:"top-12", right:"right-[5%]", color: "#0E4749"});
+const buttonLayout = new ComponentLayout({height:"", width:"", top:"top-12", right:"", color: "#0E4749"});
 const titleLayout = new ComponentLayout({height:"", width:""})
 const mailIconLayout = new ComponentLayout({height:"", width:"", left:"left-96", top:"top-4", color:"#0E4749"});
 
